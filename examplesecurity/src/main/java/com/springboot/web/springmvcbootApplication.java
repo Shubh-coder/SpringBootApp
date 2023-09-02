8c56a13302e7bc68bd6e79e1ef952515a2b64cf3
Shubh-coder <shubham.choudhary.sbg@gmail.com>
2023-09-02 23:55:26 +0530

    update hii

--- examplesecurity/src/main/java/com/springboot/web/springmvcbootApplication.java
+++ examplesecurity/src/main/java/com/springboot/web/springmvcbootApplication.java
@@ -8,6 +8,7 @@ public class springmvcbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(springmvcbootApplication.class, args);
+		System.out.println("Hii World");
 	}
 
 }
